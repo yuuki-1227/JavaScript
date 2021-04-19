c6ce6073c843b83f7e3ac06f3b9e672d91814080
yuuki-1227 <uncle.ozin.ochiai@gmail.com>
2021-04-19 15:23:49 +0900

    [Update]条件式

--- app.js
+++ app.js
@@ -1,2 +1,40 @@
 var hello = 'Hello World';
 alert(hello);
+
+var int1 = 1;
+alert(int1);
+
+var str1 = 'Hello';
+var str2 = 'World!!!'
+alert(str1 + str2);
+
+var orange = 100;
+var apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がリンゴより安い');
+} else if(orange == apple){
+  alert('みかんとリンゴが値段が同じ');
+} else{
+  alert('みかんの値段がリンゴより高い');
+}
+
+var max = 100;
+var num = 1;
+var count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です。');
+
+var i;
+var num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
